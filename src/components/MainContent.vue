9fce2963a5a7ef8a36bf5ca3084ad1ac0c1ef8f8
kanimiso <terazawa.y.270c@m.isct.ac.jp>
2025-12-12 18:39:30 +0900

    TitleとAppleたくさん

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,8 @@
 <template>
-  <div :class="$style.container"></div>
+  <div :class="$style.container">
+    <div :class="$style.title">Title</div>
+    <div>{{ 'Apple '.repeat(40).trim() }}</div>
+  </div>
 </template>
 
 <style module>
@@ -8,5 +11,10 @@
   width: 1000px;
   margin: auto;
   height: 100%;
+  padding-top: 200px;
+}
+
+.title {
+  font-size: 40px;
 }
 </style>
