b2a737af75f595f1987811f953b7e3566c0a02bb
kanimiso <terazawa.y.270c@m.isct.ac.jp>
2025-12-12 18:09:29 +0900

    ボタンの形場所

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,20 @@
 <template>
-  <div :class="$style.container"></div>
+  <div :class="$style.container">
+    <SideBarButton /><SideBarButton /><SideBarButton /><SideBarButton />
+  </div>
 </template>
+
+<script setup lang="ts">
+import SideBarButton from './SideBarButton.vue'
+</script>
 
 <style module>
 .container {
-  background-color: aqua;
+  background-color: rgb(228, 228, 228);
   width: 400px;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
---
+++ src/components/SideBarButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="$style.container"></div>
+</template>
+
+<style module>
+.container {
+  border: 1px solid red;
+  width: 100%;
+  height: 60px;
+}
+</style>
